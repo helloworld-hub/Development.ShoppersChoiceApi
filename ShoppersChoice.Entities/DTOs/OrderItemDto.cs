098b3e9128ae6ed12cafe101c695d9b3bfcc5901
helloworld-hub <prateekkanoje13@gmail.com>
2026-07-22 23:41:26 +0530

    Implemented Orders contoller and created GetOrders api.

---
+++ ShoppersChoice.Entities/DTOs/OrderItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShoppersChoice.Entities.DTOs
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
